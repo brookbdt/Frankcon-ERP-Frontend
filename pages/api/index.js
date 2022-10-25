46ca9ed336ecb5d73a98da7c441a0a58c6f29512
Brook <brook@kelaltech.com>
2022-10-25 20:40:05 +0300

    add public strapi url

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const url = "http://localhost:1337/api";
-// const url = "http://localhost:1337/api/faqs";
+// const url = "http://localhost:1337/api";
+const url = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api`;
 export const readTask = () => axios.get(`${url}/tasks`);
 export const createQuestion = (newQuestion) =>
 	axios.post(`${url}/faqs`, newQuestion);
