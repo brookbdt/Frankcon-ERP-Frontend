723b48dd0a0a7205ee6d350291c1e8c356666ad7
Brook Belihu <68384400+brookbdt@users.noreply.github.com>
2023-02-14 18:26:50 +0300

    get server side props fix

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import { useFetchUser, useFetchUserDepartment } from "../lib/authContext";
 import { readNotification } from "./api";
 import Login from "./login";
 
-export default function Home({ jwt, purchaseRequestResponse }) {
+export default function Home({ jwt, purchaseRequestResponse, error }) {
   const [response, setResponse] = useState([]);
 
   useEffect(() => {
@@ -53,241 +53,237 @@ export default function Home({ jwt, purchaseRequestResponse }) {
       res.attributes?.paymentRequest?.data?.attributes?.isApproved === "pending"
   );
 
-  return (
-    <Layout
-      jwt={jwt}
-      purchaseRequestResponse={purchaseRequestResponse}
-      user={user}
-      userDepartment={userDepartment}
-    >
-      <Head>
-        <title>Frankcon ERP Design System</title>
-        <meta name="description" content="Frankcon ERP" />
-      </Head>
+  if (error) {
+    return (
+      <Layout>
+        <p>{error}</p>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout
+        jwt={jwt}
+        purchaseRequestResponse={purchaseRequestResponse}
+        user={user}
+        userDepartment={userDepartment}
+      >
+        <Head>
+          <title>Frankcon ERP Design System</title>
+          <meta name="description" content="Frankcon ERP" />
+        </Head>
 
-      {!loading && !user ? (
-        <>
-          <Login />
-        </>
-      ) : (
-        <Stack direction="row">
-          {/* <SideBar />  */}
-          <Stack direction="column" width="80%" height="100%">
-            {/* <Navbar /> */}
-            <Box height="24px" />
-            <Stack
-              paddingLeft="48px"
-              // paddingRight="60px"
-              width="100%"
-              height="100%"
-            >
-              <Stack direction="row" gap="4px">
-                <Typography fontSize="32px" fontWeight="700" color="#0F112E">
-                  Good Morning{" "}
+        {!loading && !user ? (
+          <>
+            <Login />
+          </>
+        ) : (
+          <Stack direction="row">
+            {/* <SideBar />  */}
+            <Stack direction="column" width="80%" height="100%">
+              {/* <Navbar /> */}
+              <Box height="24px" />
+              <Stack
+                paddingLeft="48px"
+                // paddingRight="60px"
+                width="100%"
+                height="100%"
+              >
+                <Stack direction="row" gap="4px">
+                  <Typography fontSize="32px" fontWeight="700" color="#0F112E">
+                    Good Morning{" "}
+                  </Typography>
+                  <Typography fontSize="32px" fontWeight="700" color="#4339F2">
+                    {" "}
+                    {user},
+                  </Typography>
+                </Stack>
+                <Typography fontSize="16px" fontWeight="400" color="#6F7082">
+                  Please manage your inventory and upcoming tasks and projects.
                 </Typography>
-                <Typography fontSize="32px" fontWeight="700" color="#4339F2">
-                  {" "}
-                  {user},
-                </Typography>
+                <Box height="32px" />
+                <Stack direction="row" gap="12px">
+                  {userDepartment === "admin" ||
+                  "Finance" ||
+                  "Architect" ||
+                  "Human Resource" ||
+                  "Engineering" ||
+                  "Purchaser" ||
+                  "Workshop" ? (
+                    <>
+                      <Card
+                        sx={{
+                          width: "427px",
+                          height: "170px",
+                          p: 0,
+                          borderRadius: "10px",
+                        }}
+                      >
+                        <CardContent sx={{ px: "24px" }}>
+                          <Typography
+                            fontWeight="700px"
+                            fontSize="48px"
+                            color="#F35B05"
+                            // sx={{ p: 0 }}
+                          >
+                            {pendingMaterialRequest.length +
+                              pendingPaymentRequest.length +
+                              pendingPurchaseRequest.length +
+                              pendingLeaveRequest.length}
+                          </Typography>
+                          {/* </Box> */}
+                          {/* <Box height="12px" /> */}
+                          <Typography fontWeight="500px" fontSize="20px">
+                            Pending Approvals
+                          </Typography>
+                          {/* <Box height="8px" /> */}
+
+                          <Typography
+                            // sx={{ p: 0 }}
+                            fontWeight="400px"
+                            fontSize="12px"
+                            lineHeight="14px"
+                          >
+                            This number is for pending approval on purchase
+                            orders, leave requests, and more.
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </>
+                  ) : (
+                    "h"
+                  )}
+                  <Card
+                    sx={{
+                      width: "427px",
+
+                      height: "170px",
+                      p: 0,
+                      borderRadius: "10px",
+                    }}
+                  >
+                    <CardContent sx={{ px: "24px" }}>
+                      {/* <Box> */}
+                      <Typography
+                        fontWeight="700px"
+                        fontSize="48px"
+                        color="#F35B05"
+                        // sx={{ p: 0 }}
+                      >
+                        4
+                      </Typography>
+                      {/* </Box> */}
+                      {/* <Box height="12px" /> */}
+                      <Typography fontWeight="500px" fontSize="20px">
+                        Ongoing Tasks
+                      </Typography>
+                      {/* <Box height="8px" /> */}
+
+                      <Typography
+                        // sx={{ p: 0 }}
+                        fontWeight="400px"
+                        fontSize="12px"
+                        lineHeight="14px"
+                      >
+                        This number is for upcoming approval on purchase orders,
+                        leave requests, and more tasks.
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                  <Card
+                    sx={{
+                      width: "427px",
+
+                      height: "170px",
+                      p: 0,
+                      borderRadius: "10px",
+                    }}
+                  >
+                    <CardContent>
+                      {/* <Box> */}
+                      <Typography
+                        fontWeight="700px"
+                        fontSize="48px"
+                        color="#F35B05"
+                        // sx={{ p: 0 }}
+                      >
+                        8
+                      </Typography>
+                      {/* </Box> */}
+                      {/* <Box height="12px" /> */}
+                      <Typography fontWeight="500px" fontSize="20px">
+                        Purchase Orders
+                      </Typography>
+                      {/* <Box height="8px" /> */}
+
+                      <Typography
+                        // sx={{ p: 0 }}
+                        fontWeight="400px"
+                        fontSize="12px"
+                        lineHeight="14px"
+                      >
+                        This number is for pending approval on purchase orders,
+                        leave requests, and more.
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Stack>
+                <Box height="46px" />
+                <DashboardComponent jwt={jwt} />
               </Stack>
-              <Typography fontSize="16px" fontWeight="400" color="#6F7082">
-                Please manage your inventory and upcoming tasks and projects.
-              </Typography>
-              <Box height="32px" />
-              <Stack direction="row" gap="12px">
-                {userDepartment === "admin" ||
-                "Finance" ||
-                "Architect" ||
-                "Human Resource" ||
-                "Engineering" ||
-                "Purchaser" ||
-                "Workshop" ? (
-                  <>
-                    <Card
-                      sx={{
-                        width: "427px",
-                        height: "170px",
-                        p: 0,
-                        borderRadius: "10px",
-                      }}
-                    >
-                      <CardContent sx={{ px: "24px" }}>
-                        <Typography
-                          fontWeight="700px"
-                          fontSize="48px"
-                          color="#F35B05"
-                          // sx={{ p: 0 }}
-                        >
-                          {pendingMaterialRequest.length +
-                            pendingPaymentRequest.length +
-                            pendingPurchaseRequest.length +
-                            pendingLeaveRequest.length}
-                        </Typography>
-                        {/* </Box> */}
-                        {/* <Box height="12px" /> */}
-                        <Typography fontWeight="500px" fontSize="20px">
-                          Pending Approvals
-                        </Typography>
-                        {/* <Box height="8px" /> */}
-
-                        <Typography
-                          // sx={{ p: 0 }}
-                          fontWeight="400px"
-                          fontSize="12px"
-                          lineHeight="14px"
-                        >
-                          This number is for pending approval on purchase
-                          orders, leave requests, and more.
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </>
-                ) : (
-                  "h"
-                )}
-                <Card
-                  sx={{
-                    width: "427px",
-
-                    height: "170px",
-                    p: 0,
-                    borderRadius: "10px",
-                  }}
-                >
-                  <CardContent sx={{ px: "24px" }}>
-                    {/* <Box> */}
-                    <Typography
-                      fontWeight="700px"
-                      fontSize="48px"
-                      color="#F35B05"
-                      // sx={{ p: 0 }}
-                    >
-                      4
-                    </Typography>
-                    {/* </Box> */}
-                    {/* <Box height="12px" /> */}
-                    <Typography fontWeight="500px" fontSize="20px">
-                      Ongoing Tasks
-                    </Typography>
-                    {/* <Box height="8px" /> */}
-
-                    <Typography
-                      // sx={{ p: 0 }}
-                      fontWeight="400px"
-                      fontSize="12px"
-                      lineHeight="14px"
-                    >
-                      This number is for upcoming approval on purchase orders,
-                      leave requests, and more tasks.
-                    </Typography>
-                  </CardContent>
-                </Card>
-                <Card
-                  sx={{
-                    width: "427px",
-
-                    height: "170px",
-                    p: 0,
-                    borderRadius: "10px",
-                  }}
-                >
-                  <CardContent>
-                    {/* <Box> */}
-                    <Typography
-                      fontWeight="700px"
-                      fontSize="48px"
-                      color="#F35B05"
-                      // sx={{ p: 0 }}
-                    >
-                      8
-                    </Typography>
-                    {/* </Box> */}
-                    {/* <Box height="12px" /> */}
-                    <Typography fontWeight="500px" fontSize="20px">
-                      Purchase Orders
-                    </Typography>
-                    {/* <Box height="8px" /> */}
-
-                    <Typography
-                      // sx={{ p: 0 }}
-                      fontWeight="400px"
-                      fontSize="12px"
-                      lineHeight="14px"
-                    >
-                      This number is for pending approval on purchase orders,
-                      leave requests, and more.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Stack>
-              <Box height="46px" />
-              <DashboardComponent jwt={jwt} />
             </Stack>
           </Stack>
-        </Stack>
-      )}
-    </Layout>
-  );
+        )}
+      </Layout>
+    );
+  }
 }
 
 export async function getServerSideProps({ req, params }) {
   // const { slug } = params;
-  try {
-    const jwt =
-      typeof window !== "undefined"
-        ? getTokenFromLocalCookie
-        : getTokenFromServerCookie(req);
 
+  const jwt =
+    typeof window !== "undefined"
+      ? getTokenFromLocalCookie
+      : getTokenFromServerCookie(req);
+
+  const taskResponse = await fetcher(
+    `https://frankconerp.herokuapp.com/api/tasks`,
+    jwt
+      ? {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      : ""
+  );
+  console.log(1, { taskResponse });
+  const purchaseRequestResponse = await fetcher(
+    `https://frankconerp.herokuapp.com/api/purchaseRequests`,
+    jwt
+      ? {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      : ""
+  );
+  console.log(2, { purchaseRequestResponse });
+  if (taskResponse.data || purchaseRequestResponse.data) {
+    // const plot = await markdownToHtml(filmResponse.data.attributes.plot);
     return {
       props: {
+        // taskResponse: taskResponse.data,
+        // purchaseRequestResponse: purchaseRequestResponse.data,
+        // data,
+        // plot,
         jwt: jwt ? jwt : "",
       },
     };
-  } catch (e) {
-    console.log(e);
+  } else {
     return {
       props: {
-        jwt: "",
+        error: taskResponse.error.message,
       },
     };
   }
 }
-
-// const taskResponse = await fetcher(
-//   `https://frankconerp.herokuapp.com/api/tasks`,
-//   jwt
-//     ? {
-//         headers: {
-//           Authorization: `Bearer ${jwt}`,
-//         },
-//       }
-//     : ""
-// );
-// const purchaseRequestResponse = await fetcher(
-//   `https://frankconerp.herokuapp.com/api/purchaseRequests`,
-//   jwt
-//     ? {
-//         headers: {
-//           Authorization: `Bearer ${jwt}`,
-//         },
-//       }
-//     : ""
-// );
-// if (taskResponse.data || purchaseRequestResponse.data) {
-//   // const plot = await markdownToHtml(filmResponse.data.attributes.plot);
-//   return {
-//     props: {
-//       // taskResponse: taskResponse.data,
-//       // purchaseRequestResponse: purchaseRequestResponse.data,
-//       // data,
-//       // plot,
-//       jwt: jwt ? jwt : "",
-//     },
-//   };
-// } else {
-//   return {
-//     props: {
-//       error: taskResponse.error.message,
-//     },
-//   };
-// }
